598bb8e34b3479312c74fee2df7507a98361aa89
Michael W <baddaemon87@gmail.com>
2020-05-01 17:52:06 +0200

    duchamp: overlay-lineage: Disable NearbyMessagingService and DiscoveryService
    * Affects Wi-Fi speed and stability
    
    Change-Id: I12fcd4972d6b8ddb0cf9d5e0a54fb72af24947e2

--- device.mk
+++ device.mk
@@ -195,6 +195,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
    LineageApertureOverlayDuchamp \
    LineageDialerDuchamp \
+   LineageSDKOverlayDuchamp \
    LineageSettingsOverlayDuchamp
 
 # Parts
