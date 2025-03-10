f7754f5214b994b7d4152e326f15d6a2f90281ea
zenin1504 <kayozzprojects@yandex.com>
2025-03-10 07:43:49 +0000

    duchamp: Initial @AxionAOSP bringup
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,7 +165,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-12-01
+VENDOR_SECURITY_PATCH := 2025-01-01
 
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
